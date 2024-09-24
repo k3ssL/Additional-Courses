f122300085eb6d9be77c42f248d5886699cdd8f1
Aleksey <viqwer3@yandex.ru>
2024-09-25 00:56:06 +0500

    refactor: remove unused imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import MainPage from "./pages/MainPage/MainPage"
 import Header from "./components/Header/Header"
 import { BrowserRouter } from "react-router-dom"
-import BasketPage from "./pages/BasketPage/BasketPage"
 import AppRouter from "./AppRouter"
 
 function App() {
