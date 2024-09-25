d5718f91b61b750fea57e443d811e8ae8479e829
Aleksey <viqwer3@yandex.ru>
2024-09-26 02:48:54 +0500

    refactor: add alt attribute

--- src/pages/BasketPage/UI/CartItem/CartItem.jsx
+++ src/pages/BasketPage/UI/CartItem/CartItem.jsx
@@ -10,9 +10,9 @@ const CartItem = ({ img, name, type, counter, price }) => {
                 <span className={classes.item_type}>{type}</span>
             </div>
             <div className={classes.items_counter}>
-                <img src={"/img/Group 35.png"} className={classes.control_counter} />
+                <img src={"/img/Group 35.png"} className={classes.control_counter} alt={"minus-pic"} />
                 <span className={classes.counter}>{counter}</span>
-                <img src={"/img/Group 37.png"} className={classes.control_counter} />
+                <img src={"/img/Group 37.png"} className={classes.control_counter} alt={"plus-pic"} />
             </div>
             <span className={classes.item_price}>{price} ₽</span>
             <img src={"/img/Group 36.png"} alt={"delete-item-pic"} className={classes.item_delete} />
