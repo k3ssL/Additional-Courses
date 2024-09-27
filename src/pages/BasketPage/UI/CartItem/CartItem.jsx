553523d71a5a506867b7dda01a870393e99b0ab4
Aleksey <viqwer3@yandex.ru>
2024-09-28 01:02:37 +0500

    feat: add possibility increase item, decrease, delete item in cart

--- src/pages/BasketPage/UI/CartItem/CartItem.jsx
+++ src/pages/BasketPage/UI/CartItem/CartItem.jsx
@@ -1,7 +1,10 @@
 import React from "react"
 import classes from "./CartItem.module.css"
+import { useCart } from "../../../../context/CartContext"
 
-const CartItem = ({ img, name, type, counter, price }) => {
+const CartItem = ({ id, img, name, type, counter, price }) => {
+    const { increaseInCart, decreaseFromCart, removeFromCart } = useCart()
+
     return (
         <div className={classes.container}>
             <img src={img} alt={"item-pic"} className={classes.item_img} />
@@ -10,12 +13,27 @@ const CartItem = ({ img, name, type, counter, price }) => {
                 <span className={classes.item_type}>{type}</span>
             </div>
             <div className={classes.items_counter}>
-                <img src={"/img/Group 35.png"} className={classes.control_counter} alt={"minus-pic"} />
+                <img
+                    src={"/img/Group 35.png"}
+                    className={classes.control_counter}
+                    alt={"minus-pic"}
+                    onClick={() => decreaseFromCart(id)}
+                />
                 <span className={classes.counter}>{counter}</span>
-                <img src={"/img/Group 37.png"} className={classes.control_counter} alt={"plus-pic"} />
+                <img
+                    src={"/img/Group 37.png"}
+                    className={classes.control_counter}
+                    alt={"plus-pic"}
+                    onClick={() => increaseInCart(id)}
+                />
             </div>
-            <span className={classes.item_price}>{price} ₽</span>
-            <img src={"/img/Group 36.png"} alt={"delete-item-pic"} className={classes.item_delete} />
+            <span className={classes.item_price}>{price * counter} ₽</span>
+            <img
+                src={"/img/Group 36.png"}
+                alt={"delete-item-pic"}
+                className={classes.item_delete}
+                onClick={() => removeFromCart(id)}
+            />
         </div>
     )
 }
