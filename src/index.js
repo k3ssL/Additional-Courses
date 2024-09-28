4bca936b982d61dab8506d648c3cbfa4321a567c
-k3ssL <viqwer3@yandex.ru>
2024-09-28 09:44:34 +0300

    refactoring: replaced index file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import "./index.css"
-import App from "./App"
-import { CartProvider } from "../context/CartContext"
+import "./app/index.css"
+import App from "./app/App"
+import { CartProvider } from "./context/CartContext"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
