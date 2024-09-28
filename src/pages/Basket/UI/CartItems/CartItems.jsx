f90103fa9455d3b56a0768aba849608cb9aba974
-k3ssL <viqwer3@yandex.ru>
2024-09-28 08:57:08 +0300

    refactor: replaced

---
+++ src/pages/Basket/UI/CartItems/CartItems.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+import classes from "./CartItems.module.css"
+import CartItem from "../CartItem/CartItem"
+import { useCart } from "../../../../context/CartContext"
+
+const CartItems = () => {
+    const { cartItems } = useCart()
+
+    console.log(cartItems)
+
+    return (
+        <div className={classes.container}>
+            {cartItems.map((item) => (
+                <CartItem
+                    key={item.id}
+                    id={item.id}
+                    img={item.image}
+                    name={item.name}
+                    type={item.type}
+                    counter={item.counter}
+                    price={item.price}
+                />
+            ))}
+        </div>
+    )
+}
+
+export default CartItems
