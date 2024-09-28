58eb58fd49c4ce3063cc37cc74d064f4dc420d01
-k3ssL <viqwer3@yandex.ru>
2024-09-28 09:45:32 +0300

    refactoring: replaced AppRouter file

--- src/app/AppRouter.js
+++ src/app/AppRouter.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Route, Routes } from "react-router-dom"
-import { routes } from "../app/routes"
+import { routes } from "./routes"
 
 const AppRouter = () => {
     return (
