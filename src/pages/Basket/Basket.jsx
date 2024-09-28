bfddaed4f06db9595d5573640a0cc8be9d9e50ba
-k3ssL <viqwer3@yandex.ru>
2024-09-28 08:41:18 +0300

    refactor: cart logic replaced in other component

--- src/pages/Basket/Basket.jsx
+++ src/pages/Basket/Basket.jsx
@@ -1,52 +1,12 @@
 import React from "react"
-import classes from "./Basket.module.css"
 import EmptyCart from "./UI/EmptyCart/EmptyCart"
-import CartItems from "./UI/CartItems/CartItems"
-import { useNavigate } from "react-router-dom"
-import { MAIN_ROUTE, SBP_ROUTE } from "../../utils/consts"
 import { useCart } from "../../context/CartContext"
+import Cart from "./UI/Cart/Cart"
 
 const Basket = () => {
-    const { cartItems, clearCart } = useCart()
-    const navigate = useNavigate()
+    const { cartItems } = useCart()
 
-    const totalCourses = cartItems.reduce((acc, item) => acc + item.counter, 0)
-
-    const totalPrice = cartItems.reduce((acc, item) => acc + item.price * item.counter, 0)
-
-    return cartItems.length > 0 ? (
-        <div className={classes.container}>
-            <div className={classes.title}>
-                <div className={classes.cart}>
-                    <img src={"/img/iconfinder_shopping-cart_2561279 2.png"} alt={"cart-pic"} />
-                    <h1>Корзина</h1>
-                </div>
-                <div className={classes.trash} onClick={clearCart}>
-                    <img src={"/img/iconfinder_trash-2_3324927 1.png"} alt={"trash-pic"} />
-                    <p>Очистить корзину</p>
-                </div>
-            </div>
-            <hr />
-            <CartItems />
-            <div className={classes.total_courses}>
-                <span>Всего курсов: {totalCourses} шт.</span>
-                <p>
-                    Сумма заказа: <span className={classes.total_courses_price}>{totalPrice} ₽</span>
-                </p>
-            </div>
-            <div className={classes.bottom_buttons}>
-                <button onClick={() => navigate(MAIN_ROUTE)} className={classes.back_button}>
-                    <img src={"/img/Path.png"} alt={"back-arrow-pic"} />
-                    Вернуться назад
-                </button>
-                <button className={classes.pay_button} onClick={() => (window.location.href = SBP_ROUTE)}>
-                    Оплатить сейчас
-                </button>
-            </div>
-        </div>
-    ) : (
-        <EmptyCart />
-    )
+    return cartItems.length > 0 ? <Cart /> : <EmptyCart />
 }
 
 export default Basket
---
+++ src/pages/Basket/UI/Cart/Cart.jsx
@@ -0,0 +1,49 @@
+import React from "react"
+import classes from "./Cart.module.css"
+import CartItems from "../CartItems/CartItems"
+import { MAIN_ROUTE, SBP_ROUTE } from "../../../../utils/consts"
+import { useCart } from "../../../../context/CartContext"
+import { useNavigate } from "react-router-dom"
+
+const Cart = () => {
+    const { cartItems, clearCart } = useCart()
+    const navigate = useNavigate()
+
+    const totalCourses = cartItems.reduce((acc, item) => acc + item.counter, 0)
+
+    const totalPrice = cartItems.reduce((acc, item) => acc + item.price * item.counter, 0)
+
+    return (
+        <div className={classes.container}>
+            <div className={classes.title}>
+                <div className={classes.cart}>
+                    <img src={"/img/iconfinder_shopping-cart_2561279 2.png"} alt={"cart-pic"} />
+                    <h1>Корзина</h1>
+                </div>
+                <div className={classes.trash} onClick={clearCart}>
+                    <img src={"/img/iconfinder_trash-2_3324927 1.png"} alt={"trash-pic"} />
+                    <p>Очистить корзину</p>
+                </div>
+            </div>
+            <hr />
+            <CartItems />
+            <div className={classes.total_courses}>
+                <span>Всего курсов: {totalCourses} шт.</span>
+                <p>
+                    Сумма заказа: <span className={classes.total_courses_price}>{totalPrice} ₽</span>
+                </p>
+            </div>
+            <div className={classes.bottom_buttons}>
+                <button onClick={() => navigate(MAIN_ROUTE)} className={classes.back_button}>
+                    <img src={"/img/Path.png"} alt={"back-arrow-pic"} />
+                    Вернуться назад
+                </button>
+                <button className={classes.pay_button} onClick={() => (window.location.href = SBP_ROUTE)}>
+                    Оплатить сейчас
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Cart
