2b361d94f4b0ff336adf755a5762651390a1c3cf
-k3ssL <viqwer3@yandex.ru>
2024-09-30 09:33:33 +0300

    feat: create new sbp link

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,3 +1,4 @@
 export const MAIN_ROUTE = "/"
 export const BASKET_ROUTE = "/basket"
-export const SBP_ROUTE = "https://sbp.nspk.ru/?utm_source=yandex.ru&utm_medium=organic&utm_campaign=yandex.ru"
+export const SBP_ROUTE =
+    "https://qr.nspk.ru/AD10007PNOBVNVP78A4BPPCDBULN1K8R?type=02&bank=100000000111&sum=20000&cur=RUB&crc=5B32"
