5a2b025106f122f73e20f2e09e0646b25a76974e
-k3ssL <viqwer3@yandex.ru>
2024-10-02 13:10:00 +0500

    refactor: delete unused component

--- src/pages/Main/UI/Categories/Categories.jsx
+++ src/pages/Main/UI/Categories/Categories.jsx
@@ -2,7 +2,6 @@ import React from "react"
 import classes from "./Categories.module.css"
 import Category from "../Category/Category"
 import { useCategories } from "./useCategories"
-import SkeletonCategories from "../SkeletonCategories/SkeletonCategories"
 
 const Categories = ({ setActiveCategories }) => {
     const { categories, localActiveCategory, handleActiveCategory, activeCategoryName } =
@@ -11,18 +10,14 @@ const Categories = ({ setActiveCategories }) => {
     return (
         <>
             <div className={classes.container}>
-                {categories.length > 0 ? (
-                    categories.map((category) => (
-                        <Category
-                            key={category.id}
-                            name={category.name}
-                            isActive={category.id === localActiveCategory}
-                            onClick={() => handleActiveCategory(category.id)}
-                        />
-                    ))
-                ) : (
-                    <SkeletonCategories />
-                )}
+                {categories.map((category) => (
+                    <Category
+                        key={category.id}
+                        name={category.name}
+                        isActive={category.id === localActiveCategory}
+                        onClick={() => handleActiveCategory(category.id)}
+                    />
+                ))}
             </div>
             <h1 className={classes.category_name}>{activeCategoryName}</h1>
         </>
