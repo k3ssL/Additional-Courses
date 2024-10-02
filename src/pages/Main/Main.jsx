fbc8655b9a7b7593dc28d8a42a55583718d81333
Aleksey <viqwer3@yandex.ru>
2024-10-02 22:35:20 +0500

    refactor: fix styles main page

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,12 +1,11 @@
 import React, { useState } from "react"
-import classes from "./Main.module.css"
 import Categories from "./UI/Categories/Categories"
 import Cards from "./UI/Cards/Cards"
 
 const Main = () => {
     const [activeCategory, setActiveCategory] = useState(0)
     return (
-        <div className={classes.container}>
+        <div>
             <Categories setActiveCategories={setActiveCategory} />
             <Cards activeCategory={activeCategory} />
         </div>
