378434157865e697b1a18d2fce9b5e6c859b7a47
Aleksey <viqwer3@yandex.ru>
2024-10-02 22:52:23 +0500

    refactor: styles empty cart component

--- src/pages/Basket/UI/EmptyCart/EmptyCart.jsx
+++ src/pages/Basket/UI/EmptyCart/EmptyCart.jsx
@@ -7,15 +7,17 @@ const EmptyCart = () => {
     const navigate = useNavigate()
     return (
         <div className={classes.container}>
-            <h1>
-                Корзина пустая <img src={"/img/smile.png"} alt={"smile-pic"} />
+            <h1 className={classes.title}>
+                Корзина пустая <img src={"/img/smile.png"} alt={"smile-pic"} className={classes.smile} />
             </h1>
-            <p>
+            <p className={classes.not_found_text}>
                 Вероятней всего, вы не заказывали ещё курсы. <br /> Для того, чтобы заказать курсы, перейди на <br />
                 главную страницу.
             </p>
-            <img src={"/img/shopping-cart-colour 1.png"} alt={"not-found-pic"} />
-            <button onClick={() => navigate(MAIN_ROUTE)}>Вернуться назад</button>
+            <img src={"/img/shopping-cart-colour 1.png"} alt={"not-found-pic"} className={classes.not_found_img} />
+            <button onClick={() => navigate(MAIN_ROUTE)} className={classes.back_button}>
+                Вернуться назад
+            </button>
         </div>
     )
 }
