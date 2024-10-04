6c448a56be95ab411a751380652c07621fb33c37
Aleksey <viqwer3@yandex.ru>
2024-10-05 02:32:42 +0500

    feat: add api url in consts, replaced in app slice

--- src/app/consts.js
+++ src/app/consts.js
@@ -1,3 +1,4 @@
 export const MAIN_ROUTE = "/"
 export const BASKET_ROUTE = "/basket"
 export const PAY_ROUTE = "https://disk.yandex.ru/i/Eb_yFP_GCnQrWQ"
+export const API_URL = "https://66f19d744153791915518fff.mockapi.io/api"
