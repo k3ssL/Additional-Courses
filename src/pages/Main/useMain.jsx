7269a553636d8c9796508aaa371a0d4694f81efb
Aleksey <viqwer3@yandex.ru>
2024-10-05 02:34:17 +0500

    feat: possibility sort cards

---
+++ src/pages/Main/useMain.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+import { API_URL } from "../../app/consts"
+
+export const useMain = () => {
+    const [activeCategory, setActiveCategory] = useState(0)
+    const [sortType, setSortType] = useState({ name: "алфавиту", sortProperty: "alphabet" })
+
+    const [courses, setCourses] = useState([])
+    useEffect(() => {
+        axios.get(`${API_URL}/courses`).then((response) => {
+            setCourses(response.data)
+        })
+    }, [])
+
+    const sortedCourses = () => {
+        switch (sortType.sortProperty) {
+            case "alphabet":
+                return [...courses].sort((a, b) => a.name.localeCompare(b.name))
+            case "popularity":
+                return [...courses].sort((a, b) => b.popularity - a.popularity)
+            case "price":
+                return [...courses].sort((a, b) => a.price - b.price)
+            default:
+                return courses
+        }
+    }
+
+    return {
+        activeCategory,
+        setActiveCategory,
+        sortedCourses,
+        sortType,
+        setSortType,
+    }
+}
