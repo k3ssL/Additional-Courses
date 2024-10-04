afa507b50573d3e7b2f59c97da82cba8608e0eb9
Aleksey <viqwer3@yandex.ru>
2024-10-05 02:37:36 +0500

    feat: sort possibility

--- src/pages/Main/UI/Cards/Cards.jsx
+++ src/pages/Main/UI/Cards/Cards.jsx
@@ -1,28 +1,27 @@
 import React from "react"
 import classes from "./Cards.module.css"
 import Card from "../Card/Card"
-import { useCards } from "./useCards"
 import SkeletonCard from "../SkeletonCard/SkeletonCard"
 
-const Cards = ({ activeCategory }) => {
-    const { courses } = useCards()
-
+const Cards = ({ courses, activeCategory }) => {
     return (
-        <div className={classes.container}>
-            {courses.length > 0
-                ? courses
-                      .filter((course) => activeCategory === 0 || course.category === activeCategory)
-                      .map((course) => (
-                          <Card
-                              key={course.id}
-                              id={course.id}
-                              name={course.name}
-                              image={course.image}
-                              types={course.type}
-                              price={course.price}
-                          />
-                      ))
-                : [...new Array(8)].map((_, index) => <SkeletonCard key={index} />)}
+        <div>
+            <div className={classes.container}>
+                {courses.length > 0
+                    ? courses
+                          .filter((course) => activeCategory === 0 || course.category === activeCategory)
+                          .map((course) => (
+                              <Card
+                                  key={course.id}
+                                  id={course.id}
+                                  name={course.name}
+                                  image={course.image}
+                                  types={course.type}
+                                  price={course.price}
+                              />
+                          ))
+                    : [...new Array(8)].map((_, index) => <SkeletonCard key={index} />)}
+            </div>
         </div>
     )
 }
