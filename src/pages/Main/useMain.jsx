cdc0630ee54f3ed5da83dfeac82407e75e13ce27
Aleksey <viqwer3@yandex.ru>
2024-10-06 02:18:11 +0500

    feat: catch error in request get courses

--- src/pages/Main/useMain.jsx
+++ src/pages/Main/useMain.jsx
@@ -5,12 +5,17 @@ import { API_URL } from "../../app/consts"
 export const useMain = () => {
     const [activeCategory, setActiveCategory] = useState(0)
     const [sortType, setSortType] = useState({ name: "алфавиту", sortProperty: "alphabet" })
-
     const [courses, setCourses] = useState([])
+
     useEffect(() => {
-        axios.get(`${API_URL}/courses`).then((response) => {
-            setCourses(response.data)
-        })
+        axios
+            .get(`${API_URL}/courses`)
+            .then((response) => {
+                setCourses(response.data)
+            })
+            .catch((error) => {
+                console.error("Ошибка при загрузке курсов: ", error)
+            })
     }, [])
 
     const sortedCourses = () => {
